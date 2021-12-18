24d0515e241bd8c87717dc77f255c77a51750309
Aaryan <aaryan14032006@gmail.com>
2021-12-18 12:00:42 +0530

    created some endpoints

---
+++ index.mjs
@@ -0,0 +1,23 @@
+import express, { urlencoded } from "express";
+import { sendMail } from "./auth.mjs";
+
+const app = express();
+const PORT = 8080;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.end('Hello');
+});
+
+app.post('/sendmail', async (req, res) => {
+    const email = req.body.email;
+    const code = Math.floor(1000 + Math.random() * 9000);
+
+    console.log(email, code);
+    let response = await sendMail(email, code);
+
+    res.end(response);
+});
+
+app.listen(PORT);
